2e572e4cfa7b1406c577314b85492e9b998b637d
Uche Ogbu <ucheogbu95@gmail.com>
2019-02-08 13:55:52 +0100

    v1.01

--- index.php
+++ index.php
@@ -180,11 +180,93 @@
 
              </div> <!--carousel-inner ends -->
 
+             <a href="#myCarousel" class="left carousel-control" data-slide="prev"> <!--left carousel-control begins -->
+
+                <span class="glyphicon glyphicon-chevron-left"></span>
+
+                <span class="sr-only">Previous</span>
+
+             </a> <!--left carousel-control ends -->
+
+              <a href="#myCarousel" class="right carousel-control" data-slide="next"> <!--right carousel-control begins -->
+
+                <span class="glyphicon glyphicon-chevron-right"></span>
+
+                <span class="sr-only">Next</span>
+
+             </a> <!--right carousel-control ends -->
+
           </div>
 
        </div> <!--carousel slide ends  -->
 
     </div> <!--container slider ends -->
+    <div id="advantages"> <!--advantages begins -->
+
+       <div class="container"> <!--container begins -->
+
+         <div class="same-height-row"> <!--same-height-row begins -->
+
+            <div class="col-sm-4"> <!--col-sm-4 begins -->
+
+              <div class="box same-height"> <!--box same-height begins -->
+
+                <div class="icon"> <!--icon begin -->
+                     <i class="fa fa-heart"></i>
+                </div> <!--icon ends -->
+                 <h3><a href="#">Best Offer</a></h3>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+
+              </div> <!--box same-height ends -->
+
+            </div><!--col-sm-4 ends -->
+
+            <div class="col-sm-4"> <!--col-sm-4 begins -->
+
+              <div class="box same-height"> <!--box same-height begins -->
+
+                <div class="icon"> <!--icon begin -->
+                     <i class="fa fa-tag"></i>
+                </div> <!--icon ends -->
+                 <h3><a href="#">Best Prices</a></h3>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+
+              </div> <!--box same-height ends -->
+
+            </div><!--col-sm-4 ends -->
+
+            <div class="col-sm-4"> <!--col-sm-4 begins -->
+
+              <div class="box same-height"> <!--box same-height begins -->
+
+                <div class="icon"> <!--icon begin -->
+                     <i class="fa fa-thumbs-up"></i>
+                </div> <!--icon ends -->
+                 <h3><a href="#">100% Original</a></h3>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+
+              </div> <!--box same-height ends -->
+
+            </div><!--col-sm-4 ends -->
+
+         </div> <!--same-height-row ends -->
+
+
+       </div> <!--container ends -->
+
+    </div> <!--advantages ends -->
+    <div id="hot"> <!--hot begins -->
+
+       <div class="box"> <!--box begins -->
+
+           <div class="container"> <!--container begins -->
+
+
+           </div> <!--box ends -->
+
+       </div> <!--container ends -->
+
+    </div> <!--hot ends -->
 
     <script src="js/jquery-331.min.js"></script>
     <script src="js/bootstrap-337.min.js"></script>
