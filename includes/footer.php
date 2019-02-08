f20f3771a497f5352f96b7ef8ba2a8ea51082ba2
Uche Ogbu <ucheogbu95@gmail.com>
2019-02-08 18:31:11 +0100

    V1.1

---
+++ includes/footer.php
@@ -0,0 +1,122 @@
+         <div id="footer"> <!-- footer begins -->
+             <div class="container"> <!-- container begins -->
+                 <div class="row"> <!-- row begins -->
+                     <div class="col-sm-6 col-md-3"> <!-- col-sm-6 col-md-3 begins -->
+
+                        <h4>Pages</h4>
+
+                         <ul> <!-- ul begins -->
+                              <li>
+                                  <a href="shop.php">Shop</a>
+                              </li>
+                              <li>
+                                  <a href="checkout.php">My Account</a>
+                              </li>
+                              <li>
+                                  <a href="cart.php">Shopping Cart</a>
+                              </li>
+                              <li>
+                                  <a href="index.php">Contact Us </a>
+                              </li>
+                             </ul> <!-- ul ends -->
+
+                             <hr>
+
+                             <h4>User Section</h4>
+
+                             <ul> <!-- ul begins -->
+                                 <li><a href="checkout.php">Login</a></li>
+                                 <li><a href="customer_register.php">Register</a></li>
+                             </ul> <!-- ul ends -->
+
+
+                             <hr class="hidden-md hidden-lg hidden-sm">
+
+                     </div> <!-- col-sm-6 col-md-3 ends -->
+                     <div class="col-sm-6 col-md-3"> <!-- col-sm-6 col-md-3 begins -->
+                         <h4>Product Categories</h4>
+
+                         <ul> <!-- ul begins -->
+                             <li><a href="">Accessories</a></li>
+                             <li><a href=""> Jackets</a></li>
+                             <li><a href="">Shirts</a></li>
+                             <li><a href=""> Trouser</a></li>
+                             <li><a href=""> Shoes</a></li>
+                         </ul> <!-- ul ends -->
+
+                          <hr class="hidden-md hidden-lg ">
+
+
+                     </div> <!-- col-sm-6 col-md-3 ends -->
+                     <div class="col-sm-6 col-md-3"> <!-- col-sm-6 col-md-3 begins -->
+                         <h4>Find Us:</h4>
+                         <p>
+                             <strong>Ajekpako Stores</strong>
+                             <br> Port Harcourt
+                             <br> Nigeria
+                             <br> +234890234523
+                             <br>ajekpakostores@gmail.com
+                             <br><strong>Charles</strong>
+                         </p>
+                         <a href="contact.php">Check Our Contact Page</a>
+
+
+                          <hr class="hidden-md hidden-lg ">
+
+
+                     </div> <!-- col-sm-6 col-md-3 ends -->
+                    <div class="col-sm-6 col-md-3"> <!-- col-sm-6 col-md-3 begins -->
+                         <h4>Get The News</h4>
+                         <p class="text-muted">
+                            Don't Miss Our Latest Updated Products.
+                         </p>
+
+                        <form action="" method="post"> <!-- form  begins -->
+                            <div class="input-group"> <!-- input-group  begins -->
+                                <input type="text" class="form-control" name="email">
+                                <span class="input-group-btn"> <!-- input-group-btn  begins -->
+                                    <input type="submit" value="subscribe" class="btn btn-default">
+                                </span> <!-- input-group-btn  ends  -->
+                            </div> <!-- input-group  ends  -->
+                        </form> <!-- form  ends  -->
+
+
+                          <hr>
+
+                          <h4>Keep In Touch</h4>
+                          <p class="social">
+                              <a href="" class="fa fa-facebook"></a>
+                              <a href="" class="fa fa-twitter"></a>
+                              <a href="" class="fa fa-instagram"></a>
+                              <a href="" class="fa fa-google-plus"></a>
+                              <a href="" class="fa fa-envelope"></a>
+                          </p>
+
+
+                     </div> <!-- col-sm-6 col-md-3 ends -->
+
+
+                 </div><!-- row ends -->
+             </div>  <!-- container  ends -->
+         </div> <!-- footer ends -->
+         <div id="copyright"> <!-- #copyright begins -->
+             <div class="container"> <!-- container begins -->
+
+                 <div class="col-md-6"> <!-- col-md-6 begins -->
+
+                     <p class="pull-left"> &copy;2019 Ajekpako Stores    All Rights Reserved</p>
+
+                 </div> <!-- col-md-6 ends -->
+
+                 <div class="col-md-6"> <!-- col-md-6 begins -->
+
+                     <p class="pull-right"> &copy;Powered By <a href="http://www.Binarysolutionsng.com" >Binary Solutions Nigeria</a></p>
+
+                 </div> <!-- col-md-6 ends -->
+             </div> <!-- container ends -->
+         </div> <!-- #copyright ends -->
+    <script src="js/jquery-331.min.js"></script>
+    <script src="js/bootstrap-337.min.js"></script>
+</body>
+</html>
+
