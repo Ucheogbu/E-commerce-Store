765a81bea43e265d4310e792a430df4d932212fb
Uche Ogbu <ucheogbu95@gmail.com>
2019-02-09 14:20:22 +0100

    v1.3

--- shop.php
+++ shop.php
@@ -3,16 +3,22 @@ include("includes/header.php")
 ?>
 
 <div id="content"> <!--content start-->
+
     <div class="container"> <!--container start-->
+
         <div class="col-md-12"> <!--col-md-12 start-->
 
            <ul class="breadcrumb"> <!--breadcrumb start-->
                <li>
+
                <a href="index.php"> Home</a>
+
                </li>
+
                <li>
                    Shop
                </li>
+
            </ul> <!--breadcrumb ends-->
 
         </div> <!--col-md-12 ends-->
@@ -23,7 +29,262 @@ include("includes/sidebar.php")
 ?>
 
         </div> <!--col-md-3 ends-->
+
+        <div class="col-md-9"> <!--col-md-9 begins-->
+
+            <div class="box"> <!--box begins-->
+
+                <h1>Shop</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque ullam repellat fugiat corrupti, soluta non. Ipsam aspernatur, ducimus id consequatur corporis, dolorum molestiae possimus rem delectus sed quod eligendi beatae!
+                </p>
+            </div> <!--box ends-->
+
+            <div class="row"> <!--row begins-->
+                <div class="col-md-4 col-sm-6 center-responsive"> <!--col-md-4 col-sm-6 center-responsive begins-->
+
+                <div class="product">  <!--product begins -->
+
+                  <a href="details.php">
+
+                  <img class="img-responsive" src="admin_area/product_images/prod3.jpg" alt=" Product 1">
+
+                  </a>
+                  <div class="text"> <!--text begins -->
+                 <h3>
+                     <a href="details.php">
+                         Surfing Boards
+                     </a>
+                 </h3>
+
+                 <p class="price">
+                     $800
+                 </p>
+
+                 <p class="button">
+                     <a href="details.php" class="btn btn-default">
+                         View Details
+                     </a>
+                    <a href="details.php" class="btn btn-primary">
+                         <i class="fa fa-shopping-cart">
+                         Add To Cart
+                         </i>
+                     </a>
+                 </p>
+
+              </div> <!--text end s -->
+
+           </div> <!--product ends -->
+
+
+
+                </div> <!--col-md-4 col-sm-6 center-responsive ends-->
+
+                <div class="col-md-4 col-sm-6 center-responsive"> <!--col-md-4 col-sm-6 center-responsive begins-->
+
+                <div class="product">  <!--product begins -->
+
+                  <a href="details.php">
+
+                  <img class="img-responsive" src="admin_area/product_images/prod2.jpg" alt=" Product 1">
+
+                  </a>
+                  <div class="text"> <!--text begins -->
+                 <h3>
+                     <a href="details.php">
+                         Surfing Boards
+                     </a>
+                 </h3>
+
+                 <p class="price">
+                     $800
+                 </p>
+
+                 <p class="button">
+                     <a href="details.php" class="btn btn-default">
+                         View Details
+                     </a>
+                    <a href="details.php" class="btn btn-primary">
+                         <i class="fa fa-shopping-cart">
+                         Add To Cart
+                         </i>
+                     </a>
+                 </p>
+
+              </div> <!--text end s -->
+
+           </div> <!--product ends -->
+
+                </div> <!--col-md-4 col-sm-6 center-responsive ends-->
+
+                <div class="col-md-4 col-sm-6 center-responsive"> <!--col-md-4 col-sm-6 center-responsive begins-->
+
+                <div class="product">  <!--product begins -->
+
+                  <a href="details.php">
+
+                  <img class="img-responsive" src="admin_area/product_images/prod1.jpg" alt=" Product 1">
+
+                  </a>
+                  <div class="text"> <!--text begins -->
+                 <h3>
+                     <a href="details.php">
+                         Surfing Boards
+                     </a>
+                 </h3>
+
+                 <p class="price">
+                     $800
+                 </p>
+
+                 <p class="button">
+                     <a href="details.php" class="btn btn-default">
+                         View Details
+                     </a>
+                    <a href="details.php" class="btn btn-primary">
+                         <i class="fa fa-shopping-cart">
+                         Add To Cart
+                         </i>
+                     </a>
+                 </p>
+
+              </div> <!--text end s -->
+
+           </div> <!--product ends -->
+
+                </div> <!--col-md-4 col-sm-6 center-responsive ends-->
+
+                <div class="col-md-4 col-sm-6 center-responsive"> <!--col-md-4 col-sm-6 center-responsive begins-->
+
+                <div class="product">  <!--product begins -->
+
+                  <a href="details.php">
+
+                  <img class="img-responsive" src="admin_area/product_images/prod3.jpg" alt=" Product 1">
+
+                  </a>
+                  <div class="text"> <!--text begins -->
+                 <h3>
+                     <a href="details.php">
+                         Surfing Boards
+                     </a>
+                 </h3>
+
+                 <p class="price">
+                     $800
+                 </p>
+
+                 <p class="button">
+                     <a href="details.php" class="btn btn-default">
+                         View Details
+                     </a>
+                    <a href="details.php" class="btn btn-primary">
+                         <i class="fa fa-shopping-cart">
+                         Add To Cart
+                         </i>
+                     </a>
+                 </p>
+
+              </div> <!--text end s -->
+
+           </div> <!--product ends -->
+
+
+
+                </div> <!--col-md-4 col-sm-6 center-responsive ends-->
+
+                <div class="col-md-4 col-sm-6 center-responsive"> <!--col-md-4 col-sm-6 center-responsive begins-->
+
+                <div class="product">  <!--product begins -->
+
+                  <a href="details.php">
+
+                  <img class="img-responsive" src="admin_area/product_images/prod4.jpg" alt=" Product 1">
+
+                  </a>
+                  <div class="text"> <!--text begins -->
+                 <h3>
+                     <a href="details.php">
+                         Surfing Boards
+                     </a>
+                 </h3>
+
+                 <p class="price">
+                     $800
+                 </p>
+
+                 <p class="button">
+                     <a href="details.php" class="btn btn-default">
+                         View Details
+                     </a>
+                    <a href="details.php" class="btn btn-primary">
+                         <i class="fa fa-shopping-cart">
+                         Add To Cart
+                         </i>
+                     </a>
+                 </p>
+
+              </div> <!--text end s -->
+
+           </div> <!--product ends -->
+
+                </div> <!--col-md-4 col-sm-6 center-responsive ends-->
+
+                <div class="col-md-4 col-sm-6 center-responsive"> <!--col-md-4 col-sm-6 center-responsive begins-->
+
+                <div class="product">  <!--product begins -->
+
+                  <a href="details.php">
+
+                  <img class="img-responsive" src="admin_area/product_images/prod2.jpg" alt=" Product 1">
+
+                  </a>
+                  <div class="text"> <!--text begins -->
+                 <h3>
+                     <a href="details.php">
+                         Surfing Boards
+                     </a>
+                 </h3>
+
+                 <p class="price">
+                     $800
+                 </p>
+
+                 <p class="button">
+                     <a href="details.php" class="btn btn-default">
+                         View Details
+                     </a>
+                    <a href="details.php" class="btn btn-primary">
+                         <i class="fa fa-shopping-cart">
+                         Add To Cart
+                         </i>
+                     </a>
+                 </p>
+
+              </div> <!--text end s -->
+
+           </div> <!--product ends -->
+
+                </div> <!--col-md-4 col-sm-6 center-responsive ends-->
+            </div> <!--row ends-->
+
+            <center>
+                <ul class="pagination">
+                    <li><a href="#">First Page</a></li>
+                    <li><a href="#">1</a></li>
+                    <li><a href="#">2</a> </li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li><a href="#">Last Page</a></li>
+                </ul>
+            </center>
+
+
+        </div> <!--col-md-9 ends-->
+
     </div> <!--container  ends-->
+
 </div> <!--content ends-->
 
 
