b746b46403a6daee0f87027eb9905f16930f0555
Uche Ogbu <ucheogbu95@gmail.com>
2019-02-09 12:47:15 +0100

    Sidebar added

---
+++ includes/sidebar.php
@@ -0,0 +1,32 @@
+<div class="panel panel-default sidebar-menu"> <!--panel panel-default sidebar-menu start-->
+    <div class="panel-heading"> <!--panel-heading start-->
+        <h3 class="panel-title">Product Categories</h3>
+    </div> <!--panel-heading ends-->
+    <div class="panel-body"> <!--panel-body begins-->
+        <ul class="nav nav-pills nav-stacked category-menu"> <!--nav nav-pills nav-stacked category-menu begins-->
+
+           <li><a href="#">Jackets</a></li>
+           <li><a href="#">Shoes</a></li>
+           <li><a href="#">Accessories</a></li>
+           <li><a href="#">Shirts</a></li>
+           <li><a href="#">Trousers</a></li>
+
+        </ul> <!--nav nav-pills nav-stacked category-menu ends-->
+    </div> <!--panel-body ends-->
+</div> <!--panel panel-default sidebar-menu ends-->
+
+<div class="panel panel-default sidebar-menu"> <!--panel panel-default sidebar-menu start-->
+    <div class="panel-heading"> <!--panel-heading start-->
+        <h3 class="panel-title">Categories</h3>
+    </div> <!--panel-heading ends-->
+    <div class="panel-body"> <!--panel-body begins-->
+        <ul class="nav nav-pills nav-stacked category-menu"> <!--nav nav-pills nav-stacked category-menu begins-->
+
+           <li><a href="#">Men</a></li>
+           <li><a href="#">Women</a></li>
+           <li><a href="#">Kids</a></li>
+           <li><a href="#">Others </a></li>
+
+        </ul> <!--nav nav-pills nav-stacked category-menu ends-->
+    </div> <!--panel-body ends-->
+</div> <!--panel panel-default sidebar-menu ends-->
