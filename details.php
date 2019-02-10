1036d02f78c81b4d10484c0e325e61c77e69a98b
Uche Ogbu <ucheogbu95@gmail.com>
2019-02-10 19:56:05 +0100

    v1.30

---
+++ details.php
@@ -0,0 +1,39 @@
+<?php
+include("includes/header.php")
+?>
+
+    <div id="content"> <!--content start-->
+
+    <div class="container"> <!--container start-->
+
+        <div class="col-md-12"> <!--col-md-12 start-->
+
+           <ul class="breadcrumb"> <!--breadcrumb start-->
+               <li>
+
+               <a href="index.php"> Home</a>
+
+               </li>
+
+               <li>
+                   Shop
+               </li>
+
+           </ul> <!--breadcrumb ends-->
+
+        </div> <!--col-md-12 ends-->
+
+        <div class="col-md-3"> <!--col-md-3 begins -->
+<?php
+include("includes/sidebar.php")
+?>
+
+        </div> <!--col-md-3 ends-->
+</div> <!--container  ends-->
+
+</div> <!--content ends-->
+
+
+<?php
+include("includes/footer.php")
+?>
